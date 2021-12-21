c18e184b8936b2e2302badb0324062fd829e95e3
JayhsriP <96289061+JayhsriP@users.noreply.github.com>
2021-12-21 10:47:27 +0530

    Update DemoTeam1.java

--- NewTeam1/src/team1/DemoTeam1.java
+++ NewTeam1/src/team1/DemoTeam1.java
@@ -2,9 +2,15 @@ package team1;
 
 public class DemoTeam1 {
 
+	static public void m1()
+	{
+		System.out.println("this is m1 thd");
+	}
 	public static void main(String[] args) {
 		System.out.println("welcome ");
 		System.out.println("CJC ");
+		m1();
+		
 
 	}
 
