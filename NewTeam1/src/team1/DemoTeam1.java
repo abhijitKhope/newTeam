4dd3cc170a0a71d8d16fb5d657cafec2be9042ae
abhi <abhi@abhi-PC>
2021-12-21 20:05:05 +0530

    hiii

--- NewTeam1/src/team1/DemoTeam1.java
+++ NewTeam1/src/team1/DemoTeam1.java
@@ -6,16 +6,17 @@ public class DemoTeam1 {
 	{
 		System.out.println("this is m1 thd");
 	}
+	public void m2() {
+	System.out.println("m1");
+	System.out.println("github.com/JayhsriP/FirstGIT_projrct/edit/master/Git_New_project");
+	}
 	public static void main(String[] args) {
 		System.out.println("welcome ");
 		System.out.println("CJC ");
 		m1();
 		System.out.println("CJC159 ");
 
-	}
 	
-	public void m1(){
-		System.out.println("m1");
-		System.out.println("github.com/JayhsriP/FirstGIT_projrct/edit/master/Git_New_project");
+	
 	}
 }
